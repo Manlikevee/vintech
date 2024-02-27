95bfaa4c230673d4802e2baf49540373c0788887
manlikevee <odahviktor@gmail.com>
2024-02-27 18:27:41 +0100

    ppddd

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import  EvilIcons  from '@expo/vector-icons/EvilIcons';
-import  SimpleLineIcons  from '@expo/vector-icons/SimpleLineIcons';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-import { Text, View } from '@/components/Themed';
-import Avatar from '@/components/Reusables/ui/Avatar'
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
+import { Link, Tabs } from "expo-router";
+import { Pressable } from "react-native";
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { Text, View } from "@/components/Themed";
+import Avatar from "@/components/Reusables/ui/Avatar";
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof EvilIcons>['name'];
+  name: React.ComponentProps<typeof EvilIcons>["name"];
   color: string;
 }) {
-  return <EvilIcons size={28} style={{ marginBottom:2, paddingTop: 5 }} {...props} />;
+  return (
+    <EvilIcons
+      size={28}
+      style={{ marginBottom: 2, paddingTop: 5 }}
+      {...props}
+    />
+  );
 }
 
 export default function TabLayout() {
@@ -23,37 +29,37 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
         tabBarStyle: {
-          height: 62, 
+          height: 62,
           borderTopLeftRadius: 20,
           borderTopRightRadius: 20,
           paddingBottom: 10, // Adjust the padding as needed
         },
-        
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          
-          tabBarIcon: ({ color }) => <TabBarIcon name="navicon" color={color} />,
-          
+          title: "Home",
+          headerTitle: '',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="navicon" color={color} />
+          ),
+
           tabBarLabelStyle: {
-            
             fontSize: 12, // Set the font size for the label specifically for this screen
           },
           headerLeft: () => (
             <Link href="/modal" asChild>
               <Pressable>
                 {({ pressed }) => (
-<View>
-<Avatar/>
-</View>
-
+                 
+                    <Avatar />
+                
                 )}
               </Pressable>
             </Link>
@@ -65,7 +71,7 @@ export default function TabLayout() {
                   <SimpleLineIcons
                     name="logout"
                     size={18}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -77,47 +83,44 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Cards',
-          tabBarIcon: ({ color }) => <TabBarIcon name="credit-card" color={color} />,
+          title: "Cards",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="credit-card" color={color} />
+          ),
           tabBarLabelStyle: {
-
             fontSize: 12, // Set the font size for the label specifically for this screen
           },
         }}
       />
 
-<Tabs.Screen
+      <Tabs.Screen
         name="history"
         options={{
-          title: 'Payment',
+          title: "Payment",
           tabBarIcon: ({ color }) => <TabBarIcon name="link" color={color} />,
           tabBarLabelStyle: {
-
             fontSize: 12, // Set the font size for the label specifically for this screen
           },
         }}
       />
 
-<Tabs.Screen
+      <Tabs.Screen
         name="cards"
         options={{
-          title: 'History',
+          title: "History",
           tabBarIcon: ({ color }) => <TabBarIcon name="redo" color={color} />,
-                tabBarLabelStyle: {
-
-        fontSize: 12, // Set the font size for the label specifically for this screen
-      },
-
+          tabBarLabelStyle: {
+            fontSize: 12, // Set the font size for the label specifically for this screen
+          },
         }}
       />
 
-<Tabs.Screen
+      <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
+          title: "Profile",
           tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
           tabBarLabelStyle: {
-
             fontSize: 12, // Set the font size for the label specifically for this screen
           },
         }}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,41 @@
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const imageSource = { uri: 'https://uxwing.com/wp-content/themes/uxwing/download/flags-landmarks/united-states-flag-icon.png' };
   return (
-    <View style={styles.container} lightColor="#f9f9f9" darkColor="#000">
-      <Text style={styles.title}> Tab OneSss </Text>
-      <Text style={styles.fsty} lightColor="#eee" darkColor="rgba(255,255,255,0.1)">VICTOR SATOSHI </Text>
-      <Text style={styles.fstys}>VICTOR SATOSHI </Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={styles.container} lightColor="#FEFEFE" darkColor="#000">
+      <ScrollView>
+        <View style={styles.currencyinput} lightColor="#fff" darkColor="#111111">
+        <View lightColor="#fff" darkColor="#111111">
+        <Image
+          style={{
+            width: 20, // Or use '100%' for responsive width
+            height: 20,
+            resizeMode: 'contain', // Or other resize modes
+   
+          }}
+        
+        source={imageSource} />
+          </View> 
+          <View lightColor="#fff" darkColor="#111111">
+          <Text style={styles.mytxt}>USD</Text>
+          </View>
+           
+        </View>
+
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 13,
-    alignItems: 'center',
-    justifyContent: 'center',
+paddingTop:20,
+padding: 10,
+flex: 1,
     
   },
   title: {
@@ -41,5 +56,16 @@ const styles = StyleSheet.create({
     color: '#E57F06',
     fontFamily: 'Satoshimid',
     fontWeight: '100',
+  },
+  currencyinput:{
+padding: 10,
+gap:1,
+borderRadius: 9,
+borderWidth: 1,
+borderColor:  '#1C274C33',
+width: 130
+  },
+  mytxt:{
+    fontFamily: 'Satoshimid',
   }
 });
